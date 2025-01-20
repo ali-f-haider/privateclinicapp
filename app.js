51ab286e1b75ffd36327040142cfdad85f66e020
Ali Haider <ali_f_haider@hotmail.com>
2025-01-20 02:03:23 +0100

    app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
 document.addEventListener('DOMContentLoaded', () => { // Check if Firebase app is already initialized 
-  if (!firebase.apps.length) { firebase.initializeApp({ apiKey: "AIzaSyA4AwdgDl98iFD4_PA9v1lnAaN3ElEUwQ0", 
-                                                       authDomain: "privateclinicapp.firebaseapp.com", 
-                                                       projectId: "privateclinicapp", 
-                                                       storageBucket: "privateclinicapp.firebaseapp.com",   
-                                                       messagingSenderId: "968128571839", 
-                                                       appId: "1:968128571839:web:9b2be35bcd414fdf6bd90a", 
-                                                       measurementId: "G-J037W4SZHZ" }); } 
+  if (!firebase.apps.length) { firebase.initializeApp({ apiKey: "AIzaSyA4AwdgDl98iFD4_PA9v1lnAaN3ElEUwQ0",
+  authDomain: "privateclinicapp.firebaseapp.com",
+  projectId: "privateclinicapp",
+  storageBucket: "privateclinicapp.firebasestorage.app",
+  messagingSenderId: "968128571839",
+  appId: "1:968128571839:web:9b2be35bcd414fdf6bd90a",
+  measurementId: "G-J037W4SZHZ" }); } 
   else { firebase.app(); } 
   const db = firebase.firestore();
     
